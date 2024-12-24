79cbf5c2d7467435a92b24031092a6f9fff6d908
Maria <73496077+MariaShut@users.noreply.github.com>
2024-12-24 18:03:30 +0300

    initial and sass

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Player } from "./components/Player";
+
+function App() {
+  return (
+    <>
+      <Player />
+      <div className="challenges"></div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Player.jsx
@@ -0,0 +1,11 @@
+export const Player = () => {
+  return (
+    <section className="player">
+      <h2>Welcome unknown entity</h2>
+      <p>
+        <input type="text" />
+        <button>Set Name</button>
+      </p>
+    </section>
+  );
+};
